27a388c3ea6648c497c35a5430923bb9d39d3ebf
LathaVairamuthu <lathavairamuthu.1004@gmail.com>
2026-04-20 21:21:56 +0530

    Dynamic base path for Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/projects/wind/',
+    base: process.env.NODE_ENV === 'production' ? '/projects/wind/' : '/',
 })
